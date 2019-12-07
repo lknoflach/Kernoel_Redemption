a41f82fb7421174611f5837de5249ce676927fef
steins123 <48752693+steins123@users.noreply.github.com>
2019-12-07 11:54:39 +0100

    final movement

---
+++ Kernoel_Redemption3d/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    //Variables 
+    public Transform player;
+    public float smooth = 0.3f;
+
+    public float height;
+
+    private Vector3 velocity = Vector3.zero;
+    //Methods
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = new Vector3();
+        pos.x = player.position.x;
+        pos.z = player.position.z - 7f;
+        pos.y = player.position.y + height;
+        transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smooth);
+
+    }
+}
