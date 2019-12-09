190aa6f0eff5a0d0582243796451a9a49a03fa84
steins123 <48752693+steins123@users.noreply.github.com>
2019-12-09 21:42:00 +0100

    LASER !!!!!! + some models

---
+++ Kernoel_Redemption3d/Assets/Scripts/LaserScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour
+{
+    public Transform startPoint;
+    public Transform endPoint;
+    LineRenderer laserLine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        laserLine = GetComponent<LineRenderer>();
+        laserLine.SetWidth(.2f, .2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        laserLine.SetPosition(0, startPoint.position);
+        laserLine.SetPosition(1, endPoint.position);
+    }
+}
