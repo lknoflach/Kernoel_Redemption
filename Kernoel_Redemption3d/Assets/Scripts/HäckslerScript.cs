7bc2403ba2a27426695e26cd011e7bf41d48677f
steins123 <48752693+steins123@users.noreply.github.com>
2019-12-11 11:46:59 +0100

    PlayerPrefab + Traps

---
+++ Kernoel_Redemption3d/Assets/Scripts/HäckslerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HäckslerScript : MonoBehaviour
+{
+    public float x_speed = 5f;
+    public float y_speed = 5f;
+    public float z_speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(x_speed, y_speed, z_speed);
+            // Rotation amout of raotation per frame
+
+
+    }
+}
