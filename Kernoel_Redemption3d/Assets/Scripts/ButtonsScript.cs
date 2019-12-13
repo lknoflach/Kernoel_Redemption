c39e810b7b38607aaced42c09e82a9b843ed4749
steins123 <48752693+steins123@users.noreply.github.com>
2019-12-13 16:50:39 +0100

    Added Buttons and Menu

---
+++ Kernoel_Redemption3d/Assets/Scripts/ButtonsScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// Title screen script
+/// </summary>
+public class ButtonsScript : MonoBehaviour
+{
+    public void ReloadMap()
+    {
+        // "Stage1" is the name of the first scene we created.
+        SceneManager.LoadScene("StefanKLevel", LoadSceneMode.Additive);
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+}
