33dce124ab277e00af22d06d0af17dbbc3214d68
Stefan <stefan.kocher@student.tugraz.at>
2019-12-13 14:48:45 +0100

    End level message prefab

---
+++ Kernoel_Redemption3d/Assets/Scripts/EndLevelMessage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevelMessage : MonoBehaviour
+{
+  
+    private bool showName = false; 
+
+
+    void OnTriggerEnter(Collider other){
+        //sets display message to true an displays the message
+        if(other.gameObject.CompareTag("Player")){
+            //if you want to load a new scene when you reached the goal to it here
+            Debug.Log("END");
+            showName = true;
+        }
+    }
+         void OnGUI()
+     {
+         if(showName)
+             GUI.Label(new Rect(400,200,2000,2000), "You finished the level! :)");
+     }
+
+}
