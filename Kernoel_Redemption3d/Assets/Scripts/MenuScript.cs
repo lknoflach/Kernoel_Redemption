bec750a5ba1459f3ff5a98511bb1c0c46cdd665d
Claudio Kirchmair <claudio@itell.solutions>
2019-12-14 00:10:52 +0100

    Renamed MenuScript and added input for keys ("r" to StartGame and "t" to QuitGame).

---
+++ Kernoel_Redemption3d/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class MenuScript : MonoBehaviour
+{
+    private void Update()
+    {
+        // Button "r": Restart the Game
+        if (Input.GetKeyDown("r")) StartGame();
+        // Button "t": Terminate the Game
+        if (Input.GetKeyDown("t")) QuitGame();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("StefanKLevel");
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+}
