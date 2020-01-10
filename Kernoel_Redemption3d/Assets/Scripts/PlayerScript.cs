d7046c44b6def3240b6e3d9b889e266c23e6fbb0
Stefan <stefan.kocher@student.tugraz.at>
2020-01-10 20:42:16 +0100

    changed that cloning is only posible if player has kernöl and that theire is a posibility that the clone is a zombie

--- Kernoel_Redemption3d/Assets/Scripts/PlayerScript.cs
+++ Kernoel_Redemption3d/Assets/Scripts/PlayerScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerScript : MonoBehaviour
 {
     /** GUN STUFF **/
@@ -9,14 +10,18 @@ public class PlayerScript : MonoBehaviour
 
     /** MOVEMENT STUFF **/
     private Camera mainCamera;
-    public CharacterMovement characterMovement;
+    public CharacterMovement characaterMovement;
 
     /** CLONING STUFF **/
-    // enables/disables cloning
-    private bool isCloneable;
+    //checks if the player is standing on the cloningplattform
+    private bool stands_on_cloningplattform;
 
     // the prototype for new clone objects
     public GameObject clonePrototype;
+
+    // the prototype for new zombie objects
+       // the prototype for new clone objects
+    public GameObject zombiePrototype;
 
     // the object which enables the cloning
     public GameObject cloningCapsule;
@@ -24,12 +29,28 @@ public class PlayerScript : MonoBehaviour
     // the array with all the following clones
     public List<GameObject> clones = new List<GameObject>();
 
+    //amount of good kernöl
+    public int high_grade_kernoel = 0;
+    
+    //amout of kernöl with less quality is more likely to produce a zombie clone 
+    public int low_grade_kernoel = 0;
+    
+    //the likelynes that the clone created turnes out to be a zombie with good kernoel
+    public float propability_of_mutation_good_kernoel;
+    
+    //the likelynes that the clone created turnes out to be a zombie with bad kernoel
+    public int propability_of_mutation_bad_kernoel;
+
+    //just to create random numbers 
+    private Random random;
+
     private void Start()
     {
-        characterMovement = GetComponent<CharacterMovement>();
+        characaterMovement = GetComponent<CharacterMovement>();
         mainCamera = FindObjectOfType<Camera>();
         cloningCapsule.GetComponent<Transform>();
         gunFiringScript = playerGun.GetComponent<GunFiring>();
+        random = new Random();
     }
 
     private void OnCollisionEnter(Collision other)
@@ -40,6 +61,18 @@ public class PlayerScript : MonoBehaviour
             case "Clone":
                 // add the clone to the list of clones
                 if (!clones.Contains(target)) clones.Add(target);
+                break;
+            //mabe should be added in on trigger not sure 
+            //add the Kernöl to the player if he touches one 
+            case "gutesKernoel":
+                high_grade_kernoel++;
+                Debug.Log("picked up high grade Kernoel0");
+                Destroy(other.gameObject);
+                break;  
+            case "schelchtesKernoel":
+                Debug.Log("picked up low grade Kernoel");
+                low_grade_kernoel++; 
+                Destroy(other.gameObject);
                 break;
         }
     }
@@ -52,7 +85,7 @@ public class PlayerScript : MonoBehaviour
         {
             case "CloningCapsule":
                 // enable cloning
-                isCloneable = true;
+                stands_on_cloningplattform = true;
                 break;
         }
     }
@@ -64,21 +97,55 @@ public class PlayerScript : MonoBehaviour
         {
             case "CloningCapsule":
                 // enable cloning
-                isCloneable = false;
+                stands_on_cloningplattform = false;
                 break;
         }
     }
 
+    //just a random boolean function with a set likelness that it turnes out to be false 
+    public bool randomBoolean(float likelynes_in_percent){
+        
+        //return true if the random number between 0 and 100 is smaller then your likelines for a zombie
+        return (UnityEngine.Random.Range(0f, 100f) > likelynes_in_percent);
+        
+    }
+
+
+    //creates the clone if we have Kernöl and with a posibility that it turnes out to be a zombie 
+    public void createClone(){
+        if(high_grade_kernoel > 0)
+        {
+            var spawnPosition = transform.position;
+            spawnPosition.z -= 3;
+            
+            if(randomBoolean(propability_of_mutation_good_kernoel)){
+                Instantiate(clonePrototype, spawnPosition, transform.rotation);
+            }else{
+                Instantiate(zombiePrototype, spawnPosition, transform.rotation);
+            }
+            
+            high_grade_kernoel--;
+        }else if(low_grade_kernoel > 0){
+            var spawnPosition = transform.position;
+            spawnPosition.z -= 3;
+
+             if(randomBoolean(propability_of_mutation_bad_kernoel)){
+                Instantiate(clonePrototype, spawnPosition, transform.rotation);
+            }else{
+                Instantiate(zombiePrototype, spawnPosition, transform.rotation);
+            }
+            low_grade_kernoel--;
+        }
+    }
+    
+  
+
     public void Update()
     {
         //cloning Button
-        if (Input.GetKeyDown(KeyCode.E) && isCloneable)
+        if (Input.GetKeyDown(KeyCode.E) && stands_on_cloningplattform)
         {
-            var clonePosition = transform.position;
-            clonePosition.z -= 3;
-            Instantiate(clonePrototype, clonePosition, transform.rotation);
-            // disable cloning
-           //isCloneable = false;
+           createClone();
         }
 
         // look to the cursor
