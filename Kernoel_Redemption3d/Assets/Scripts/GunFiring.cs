75126025758ec5c85d1bb885398aebdcad4bf0b9
Stefan <stefan.kocher@student.tugraz.at>
2020-01-13 23:23:23 +0100

    changed the bullet to a particle system for looks

--- Kernoel_Redemption3d/Assets/Scripts/GunFiring.cs
+++ Kernoel_Redemption3d/Assets/Scripts/GunFiring.cs
@@ -10,15 +10,16 @@ public class GunFiring : MonoBehaviour
 
     // The Projectile itself
     public GameObject projectilePrefab;
-
+    
     public void Shoot()
     {
         // var source = transform.parent.gameObject;
         // var playerScript = FindObjectOfType<PlayerScript>();
         // var playerPosition = playerScript.gameObject.transform.position;
         // Debug.DrawLine(firingPoint.position, playerPosition, Color.green);
-
+       // particles.Pause(true);
         // Create a Projectile
+     
         Instantiate(projectilePrefab, firingPoint.position, firingPoint.rotation);
     }
 }
