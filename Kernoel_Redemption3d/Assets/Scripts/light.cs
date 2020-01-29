7130d63f9f018845a02fa0b5606e1e570b8abfb1
steins123 <48752693+steins123@users.noreply.github.com>
2020-01-29 16:50:35 +0100

    Level1

---
+++ Kernoel_Redemption3d/Assets/Scripts/light.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light : MonoBehaviour
+{
+    Light testlight;
+    public float minWaitTime;
+    public float maxWaitTime;
+
+    void Start()
+    {
+        testlight = GetComponent<Light>();
+        StartCoroutine(Flashing());
+    }
+
+    // Update is called once per frame
+    IEnumerator Flashing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+        }
+    }
+}
