5c5bdcd17315139551f7e6e3cce1e94ce9c3abed
steins123 <48752693+steins123@users.noreply.github.com>
2020-02-17 11:42:19 +0100

    level2

---
+++ Kernoel_Redemption3d/Assets/Animations/EasterEgg/TalkSkript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkSkript : MonoBehaviour
+{
+    GameObject text;
+    public bool isNearButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isNearButton = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        var target = other.gameObject;
+        switch (target.tag)
+        {
+            case "Player":
+                // enable cloning
+                isNearButton = true;
+                break;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var target = other.gameObject;
+        switch (target.tag)
+        {
+            case "Player":
+                // enable cloning
+                isNearButton = false;
+                break;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //cloning Button
+        if (Input.GetKeyDown(KeyCode.E) && isNearButton)
+        {
+       
+        }
+     
+    }
+}
---
+++ Kernoel_Redemption3d/Assets/Scripts/DestroyGlassScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyGlassScript : MonoBehaviour
+{
+    public GameObject Glass;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var target = other.gameObject;
+        switch (target.tag)
+        {
+            case "Bullet":
+                // enable cloning
+                Destroy(gameObject);
+                break;
+        }
+    }
+
+   
+}
