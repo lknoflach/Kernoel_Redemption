08639532c842e7b46e70bb15cbd7abc28459ede9
steins123 <48752693+steins123@users.noreply.github.com>
2020-02-19 12:54:24 +0100

    changes

---
+++ Kernoel_Redemption3d/Assets/Animations/EasterEgg/StopAudioScript1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class StopAudioScript1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject player;
+    AudioSource audioData;
+    public bool enter;
+    void Start()
+    {
+        enter = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+        audioData = GetComponent<AudioSource>();
+    }
+
+ 
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            audioData.Stop();
+
+        }
+    }
+
+
+}
