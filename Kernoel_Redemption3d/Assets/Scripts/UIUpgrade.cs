514353ae6bf2e4c78d7770b95876fa131708f6de
Stefan <stefan.kocher@student.tugraz.at>
2020-02-22 20:38:31 +0100

    Upgrade UI for speed

--- Kernoel_Redemption3d/Assets/Scripts/UIUpgrade.cs
+++ Kernoel_Redemption3d/Assets/Scripts/UIUpgrade.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIUpgrade : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject player; 
     CharacterMovement characterMovement;
-    
+    public float totalSpeed = 12f;
+    CloningScript cloningScript;
+
+    [Header("Unity Stuff")] public Image speedBar;
+   
     void Start()
     {
-         characterMovement = player.GetComponent<CharacterMovement>();
+        characterMovement = player.GetComponent<CharacterMovement>();
+        cloningScript = player.GetComponent<CloningScript>();
+        if (speedBar) speedBar.fillAmount = ((float) characterMovement.speed) / ((float) totalSpeed);
     }
 
     // Update is called once per frame
@@ -20,6 +27,11 @@ public class UIUpgrade : MonoBehaviour
     }
 
     public void upgradeSpeed(){
-        characterMovement.speed = characterMovement.speed + 1f;
+        if(characterMovement.speed < totalSpeed && cloningScript.low_grade_kernoel >= 3){
+            cloningScript.low_grade_kernoel = cloningScript.low_grade_kernoel - 3;
+            characterMovement.speed = characterMovement.speed + 1f;
+             if (speedBar) speedBar.fillAmount = ((float) characterMovement.speed) / ((float) totalSpeed);
+
+        }
     }
 }
