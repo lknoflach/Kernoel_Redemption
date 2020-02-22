292c44308ce84a244d6f4f1b2cb00b474803ce31
Claudio Kirchmair <claudio@itell.solutions>
2020-02-22 23:10:33 +0100

    Fixed Animation scripts and moved them to a separate folder/namespace.

---
+++ Kernoel_Redemption3d/Assets/Scripts/Animation/CloneAnimationScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Animation
+{
+    public class CloneAnimationScript : MonoBehaviour
+    {
+        private Animator _animator;
+        private CloneScript _cloneScript;
+        private static readonly int _isWalking = Animator.StringToHash("isWalking");
+
+        private void Start()
+        {
+            _cloneScript = transform.parent.GetComponent<CloneScript>();
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            _animator.SetBool(_isWalking, _cloneScript.isArrivedAtPlayer == false);
+        }
+    }
+}
