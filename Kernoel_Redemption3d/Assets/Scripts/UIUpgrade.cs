394960578dd22b44b3cf2dbbe304002e17d4c7e4
Stefan <stefan.kocher@student.tugraz.at>
2020-02-22 19:15:26 +0100

    speed is now upgradable

---
+++ Kernoel_Redemption3d/Assets/Scripts/UIUpgrade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIUpgrade : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player; 
+    CharacterMovement characterMovement;
+    
+    void Start()
+    {
+         characterMovement = player.GetComponent<CharacterMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void upgradeSpeed(){
+        characterMovement.speed = characterMovement.speed + 1f;
+    }
+}
