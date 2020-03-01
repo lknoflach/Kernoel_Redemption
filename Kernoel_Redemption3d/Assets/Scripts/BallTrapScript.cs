2611483e68571d001168808353c2fcbd52c4aef6
steins123 <48752693+steins123@users.noreply.github.com>
2020-03-01 17:17:52 +0100

    added BallTrapScript

---
+++ Kernoel_Redemption3d/Assets/Scripts/BallTrapScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTrapScript : MonoBehaviour
+{
+    // the array with all the traps
+    public List<GameObject> traps = new List<GameObject>();
+    private bool _isNearButton;
+
+    public void SwitchTraps()
+    {
+        foreach (var trap in traps)
+        {
+            Destroy(trap);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var target = other.gameObject;
+        switch (target.tag)
+        {
+            case "Ball":
+                // enable cloning
+                _isNearButton = true;
+                break;
+        }
+    }
+
+
+
+    private void Update()
+    {
+        if (_isNearButton)
+        {
+            // destroy the traps
+            SwitchTraps();
+        }
+    }
+}
