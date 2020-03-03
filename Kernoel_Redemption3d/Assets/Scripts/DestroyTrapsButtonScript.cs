a1fb19f69d69363888ab003932dbb4a6045e2084
Claudio Kirchmair <claudio@itell.solutions>
2020-03-03 17:29:57 +0100

    Added buttonColor to SwitchButtonOff Prefab.

--- Kernoel_Redemption3d/Assets/Scripts/DestroyTrapsButtonScript.cs
+++ Kernoel_Redemption3d/Assets/Scripts/DestroyTrapsButtonScript.cs
@@ -6,13 +6,23 @@ public class DestroyTrapsButtonScript : MonoBehaviour
     // the array with all the traps
     public List<GameObject> traps = new List<GameObject>();
     private bool _isNearButton;
+    public bool isActivated = true;
+    private GameObject _button;
 
-    public void SwitchTraps()
+    private void Start()
     {
-        foreach(var trap in traps)
-        {
-            trap.SetActive(!trap.activeSelf);
+        _button = transform.Find("Button").gameObject;
+        SetButtonColor();
+    }
+
+    public void DisableTraps()
+    {
+        foreach(var trap in traps) trap.SetActive(false);
         }
+    
+    public void EnableTraps()
+    {
+        foreach(var trap in traps) trap.SetActive(true);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -38,12 +48,25 @@ public class DestroyTrapsButtonScript : MonoBehaviour
         }
     }
 
+    private void SetButtonColor()
+    {
+        if (!_button) return;
+
+        var color = isActivated ? Color.green : Color.red;
+        var buttonLight = _button.GetComponentInChildren<Light>();
+        if (buttonLight) buttonLight.color = color;
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && _isNearButton)
-        {
-            // destroy the traps
-            SwitchTraps();
-        }
+        if (!Input.GetKeyDown(KeyCode.E) || !_isNearButton) return;
+        
+        // update the state
+        isActivated = !isActivated;
+        SetButtonColor();
+
+        // enable/disable the traps
+        if (isActivated) EnableTraps();
+        if (!isActivated) DisableTraps();
     }
 }
