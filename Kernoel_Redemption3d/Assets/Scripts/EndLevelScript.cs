b737f83a8c2940c7ae6cee83282867f68d7683f1
steins123 <48752693+steins123@users.noreply.github.com>
2020-03-05 11:34:27 +0100

    highScore End Level

--- Kernoel_Redemption3d/Assets/Scripts/EndLevelScript.cs
+++ Kernoel_Redemption3d/Assets/Scripts/EndLevelScript.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
-
+using UnityEngine.UI;
+using System.Collections;
 
 public class EndLevelScript : MonoBehaviour
 {
     private GameManager _gameManager;
-
+    public Text kernölAmountText;
     private void Start()
     {
         _gameManager = GameManager.Instance;
+        kernölAmountText.text = "Kernöl: " + GameManager.Instance.KernoilScore;
+        updateUI();
     }
     
     public void LoadMainMenu()
@@ -24,4 +27,11 @@ public class EndLevelScript : MonoBehaviour
     {
         _gameManager.QuitGame();
     }
+
+    public void updateUI()
+    {
+        kernölAmountText.text = "Kernöl: " + GameManager.Instance.KernoilScore;
+    }
+
+
 }
