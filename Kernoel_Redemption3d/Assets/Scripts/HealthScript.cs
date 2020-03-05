2caf8bb7f379fc125444b778219c24075aad188d
Claudio Kirchmair <claudio@itell.solutions>
2020-03-05 14:54:50 +0100

    Refactored Seed Oil. Refactored HealthScript. Adapted Seed Oil spawn rates for all Characters.

--- Kernoel_Redemption3d/Assets/Scripts/HealthScript.cs
+++ Kernoel_Redemption3d/Assets/Scripts/HealthScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 /// <summary>
@@ -8,23 +9,32 @@ using UnityEngine.UI;
 public class HealthScript : MonoBehaviour
 {
     /** HEALTH STUFF **/
+    [Header("Life")]
     public int currentHealth = 100;
     public int totalHealth = 100;
-    public GameObject DeathEffect;
-    public float timeOut = 3.0f;
-    public CloningScript kernoil;
-    public float probabilityOfMutationGoodSeedOil;
-    [Header("Unity Stuff")] public Image healthBar;
-    public GameObject Kernoil;
-    public GameObject noKernoil;
 
+    [Header("Unity Stuff")]
+    public Image healthBar;
+    
+    [Header("Death")]
+    [FormerlySerializedAs("DeathEffect")]public GameObject deathEffect;
+    [FormerlySerializedAs("timeOut")] public float deathEffectTimer = 3.0f;
+    [FormerlySerializedAs("Kernoil")] public GameObject seedOil;
+    [FormerlySerializedAs("probabilityOfMutationGoodSeedOil")] public float seedOilDropChance;
+    
     private void Start()
     {
         // Set currentHealth to totalHealth
         currentHealth = totalHealth;
 
         // Update the health bar
-        if (healthBar) healthBar.fillAmount = ((float) currentHealth) / ((float) totalHealth);
+        if (healthBar) healthBar.fillAmount = (float) currentHealth / totalHealth;
+    }
+
+    private static bool RandomBoolean(float likelinessInPercent)
+    {
+        // return true if the random number between 0 and 100 is smaller then your likeliness for a zombie
+        return (Random.Range(0f, 100f) > likelinessInPercent);
     }
 
     /// <summary>
@@ -32,49 +42,49 @@ public class HealthScript : MonoBehaviour
     /// </summary>
     /// <param name="damageCount"></param>
     /// 
-    private static bool RandomBoolean(float likelinessInPercent)
-    {
-        // return true if the random number between 0 and 100 is smaller then your likeliness for a zombie
-        return (Random.Range(0f, 100f) > likelinessInPercent);
-    }
-
     public void Damage(int damageCount)
     {
         currentHealth -= damageCount;
         //Debug.Log(gameObject.name + ": currentHealth<" + currentHealth.ToString() + ">");
 
         // Update the health bar
-        if (healthBar) healthBar.fillAmount = ((float) currentHealth) / ((float) totalHealth);
+        if (healthBar) healthBar.fillAmount = (float) currentHealth / totalHealth;
 
         if (currentHealth > 0) return;
 
        // Dead!
-         var isGameOver = gameObject.name == "PlayerHans";
+       var isGameOver = gameObject.name == "PlayerHans";
 
-        var spawnPosition = transform.position;
-        spawnPosition.z -= 3;
+       var spawnPosition = transform.position;
+       spawnPosition.z -= 3;
 
-        var instance = Instantiate(DeathEffect, transform.position, transform.rotation);
-        Destroy(instance.gameObject, timeOut);
+        // Show Death Effect if present
+        if (deathEffect)
+        {
+            var instance = Instantiate(deathEffect, transform.position, transform.rotation);
+            Destroy(instance.gameObject, deathEffectTimer);
+        }
+
         Destroy(gameObject);
 
-        Instantiate(RandomBoolean(probabilityOfMutationGoodSeedOil) ? Kernoil : noKernoil, spawnPosition, transform.rotation);
-
-
-
-        if (isGameOver)
+        // Spawn Seed Oil if present and there is a lucky fairy which says YES!
+        if (seedOil && RandomBoolean(seedOilDropChance))
         {
-            // Set activeScene as currentScene before loading GameOverMenu
-            var activeScene = SceneManager.GetActiveScene();
-            GameManager.Instance.currentSceneName = activeScene.name;
-            GameManager.Instance.LoadGameOverMenu();
+            Instantiate(seedOil, spawnPosition, transform.rotation);
         }
+
+        if (!isGameOver) return;
+        
+        // Set activeScene as currentScene before loading GameOverMenu
+        var activeScene = SceneManager.GetActiveScene();
+        GameManager.Instance.currentSceneName = activeScene.name;
+        GameManager.Instance.LoadGameOverMenu();
     }
 
-    private void OnGUI()
+    /*private void OnGUI()
     {
         // [CK] Another way to display a health bar would be this
-        // var targetPos = Camera.main.WorldToScreenPoint(transform.position);
-        // GUI.Box(new Rect(targetPos.x, Screen.height - targetPos.y, 60, 20), currentHealth + "/" + totalHealth);
-    }
+        var targetPos = Camera.main.WorldToScreenPoint(transform.position);
+        GUI.Box(new Rect(targetPos.x, Screen.height - targetPos.y, 60, 20), currentHealth + "/" + totalHealth);
+    }*/
 }
