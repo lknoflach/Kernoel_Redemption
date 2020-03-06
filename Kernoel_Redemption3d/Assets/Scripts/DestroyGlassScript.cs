573124761a810d73924fb286dfd04fd8275a69f4
Claudio Kirchmair <claudio@itell.solutions>
2020-03-06 12:52:49 +0100

    Added sounds to Glass and SeedOil.

--- Kernoel_Redemption3d/Assets/Scripts/DestroyGlassScript.cs
+++ Kernoel_Redemption3d/Assets/Scripts/DestroyGlassScript.cs
@@ -1,13 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class DestroyGlassScript : MonoBehaviour
 {
+    private AudioSource _audioData; 
+
+    private void Start()
+    {
+        _audioData = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         var target = other.gameObject;
         switch (target.tag)
         {
             case "Bullet":
+                if (_audioData) _audioData.Play();
                 Destroy(gameObject);
                 break;
         }
